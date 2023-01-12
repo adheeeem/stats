7339368af54d8c23a52511017ffdec8911145fc7
adheeeem <sherman.peabody99@gmail.com>
2023-01-12 10:40:27 +0500

    updated to the last version

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
-module github.com/adheeeem/stats
+module github.com/adheeeem/stats/v2
 
 go 1.19
 
-require github.com/adheeeem/bank v0.0.0-20230111172651-a2ffc414185d // indirect
+require github.com/adheeeem/bank/v2 v2.0.0
--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -1,12 +1,14 @@
 package stats
 
-import "github.com/adheeeem/bank/pkg/types"
+import "github.com/adheeeem/bank/v2/pkg/types"
 
 func Avg(payments []types.Payment) types.Money {
 	length := len(payments)
 	sum := types.Money(0)
 	for _, payment := range payments {
-		sum += payment.Amount
+		if payment.Status != types.StatusFail {
+			sum += payment.Amount
+		}
 	}
 	sum /= types.Money(length)
 	return sum
@@ -14,7 +16,7 @@ func Avg(payments []types.Payment) types.Money {
 func TotalInCategory(payments []types.Payment, category types.Category) types.Money {
 	total := types.Money(0)
 	for _, payment := range payments {
-		if payment.Category == category {
+		if payment.Category == category && payment.Status != types.StatusFail {
 			total += payment.Amount
 		}
 	}
